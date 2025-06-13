8509cb3b45f9859a8c383821d75bc9d407da1955
Noé <noe.morance.1@ens.etsmtl.ca>
2025-06-12 22:18:47 -0400

    ajout de la méthode ajouter_profil pour enregistrer un profil dans la base de données

--- gestion_base.py
+++ gestion_base.py
@@ -31,3 +31,19 @@ class GestionBase:
              if not os.path.exists(self.chemin_fichier):  #dans ce cas on creer un tableau vide avec les bonnes colonnes
                  df=pd.DataFrame(columns=self.colonnes)
                  df.to_csv(self.chemin_fichier, index=False)
+
+        def ajouter_profil(self, nom_profil, type_profil,
+                           fichier_coord_csv=None, fichier_coord_dat=None,fichier_polaire_txt=None,fichier_polaire_csv=None):
+
+           """
+           Ajoute un nouveau profil à la base de données avec les chemins associés aux fichiersgénérés
+
+           :param nom_profil: nom du profil
+           :param type_profil: manuel ou importé
+           :param fichier_coord_csv: chemin vers le fichier csv des coordonnées
+           :param fichier_coord_dat: chemins vers le fichier .dat de XFOIL
+           :param fichier_polaire_txt:chemin vers le fichier texte , résultats de XFOIL
+           :param fichier_polaire_csv: chemin vers le fichier csv des polaires de AirfoilTools
+           """
+
+           df=pd.read_csv(self.chemin_fichier)
