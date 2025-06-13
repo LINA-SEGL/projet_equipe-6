87dd13f8676a3433089ef6640b2fe3a114629903
Noé <noe.morance.1@ens.etsmtl.ca>
2025-06-12 22:08:26 -0400

    ajout de la méthode _initialiser_fichier

--- gestion_base.py
+++ gestion_base.py
@@ -15,9 +15,19 @@ class GestionBase:
              "nom_profil",
              "type_profil", # importé ou manuel
              "date_creation",
-             "fihchier_coord_csv",
+             "fichier_coord_csv",
              "fichier_coord_dat",
              "fichier_polaire_txt",
              "fichier_polaire_csv"
          ]
-         self.initialiser_fichier
+         self.initialiser_fichier()
+
+        def _initialiser_fichier(self):
+             """
+             Creé le dossier 'data' et le fichier 'donnees_profils.csv' s'ils n'existent pas.
+             """
+             os.makedirs(self.chemin_dossier,exist_ok=True) #on crée le dossier que il n'est pas déja présent
+
+             if not os.path.exists(self.chemin_fichier):  #dans ce cas on creer un tableau vide avec les bonnes colonnes
+                 df=pd.DataFrame(columns=self.colonnes)
+                 df.to_csv(self.chemin_fichier, index=False)
